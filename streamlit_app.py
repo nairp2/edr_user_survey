8dd33d30ce929bcb943a1e822866132175aa0241
Pranav Nair <nairp2@tcnj.edu>
2026-09-22 14:29:52 -0400

    Font Title change

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,11 +115,7 @@ if page == "📝 Take Survey":
             line-height:1.6;
         ">
         <br>
-        Thank you for taking the time to share your input on the CRM Tableau dashboards. Your insights drive dashboard content and design helping us provide better tools for your work.
-        
-        <br>
-
-        <strong>Please complete all sections in this survey as thoroughly as possible.</strong>
+        Thank you for taking the time to share your input on the CRM Tableau dashboards. Your insights drive dashboard content and design helping us provide better tools for your work. <strong>Please complete all sections in this survey as thoroughly as possible.</strong>
 
         <br>
 
